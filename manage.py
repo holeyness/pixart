c568d27cc86f3af32c6e34df1282533cc62e1d43
Jon Arbaugh <jon@acenna.com>
2019-02-01 22:45:11 -0500

    Add env file loading

--- manage.py
+++ manage.py
@@ -3,6 +3,9 @@ import os
 import sys
 
 if __name__ == '__main__':
+    from dotenv import load_dotenv, find_dotenv
+    load_dotenv(find_dotenv())
+
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pixart.settings')
     try:
         from django.core.management import execute_from_command_line
